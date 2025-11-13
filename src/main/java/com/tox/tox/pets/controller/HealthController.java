256ab6737555c3f1d8e58e8aba8a8dc98b17df4b
方思维 <ywfswywfsw@gmail.com>
2025-11-13 16:54:06 +0800

    feat: 保活接口

---
+++ src/main/java/com/tox/tox/pets/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.tox.tox.pets.controller; // (替换成你的包名)
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 健康检查 / 保活接口
+ */
+@RestController
+public class HealthController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong"; 
+    }
+}
