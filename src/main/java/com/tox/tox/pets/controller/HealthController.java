4ef3d0757e7dede1d25111fb365eeb00b1a4050e
ToxicTox <ywfswywfsw@gmail.com>
2025-11-18 10:43:33 +0800

    feat:ping禁止缓存

--- src/main/java/com/tox/tox/pets/controller/HealthController.java
+++ src/main/java/com/tox/tox/pets/controller/HealthController.java
@@ -1,6 +1,7 @@
-package com.tox.tox.pets.controller; // (替换成你的包名)
+package com.tox.tox.pets.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
+    /**
+     * (❗ 升级版：增加了 Cache-Control 响应头)
+     */
     @GetMapping("/ping")
-    public String ping() {
-        return "pong"; 
+    public ResponseEntity<String> ping() {
+
+        // (❗) 构造 Cache-Control: "no-store, no-cache, must-revalidate"
+        // (这是最强的“禁止缓存”指令)
+        CacheControl cacheControl = CacheControl
+                .noStore()
+                .mustRevalidate();
+
+        // (❗) 返回 200 OK, "pong", 和禁止缓存的 Header
+        return ResponseEntity.ok()
+                .cacheControl(cacheControl)
+                .body("pong");
     }
 }
